fc88c9654e6f9028491598b5d9b0bc50d073f11f
Shaurya Gupta <111460487+shaurya2093@users.noreply.github.com>
2024-08-06 01:53:28 +0530

    Update dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -83,23 +83,4 @@ void dijkstra(int graph[V][V], int src)
 	printSolution(dist);
 }
 
-// driver's code
-int main()
-{
 
-	/* Let us create the example graph discussed above */
-	int graph[V][V] = { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
-						{ 4, 0, 8, 0, 0, 0, 0, 11, 0 },
-						{ 0, 8, 0, 7, 0, 4, 0, 0, 2 },
-						{ 0, 0, 7, 0, 9, 14, 0, 0, 0 },
-						{ 0, 0, 0, 9, 0, 10, 0, 0, 0 },
-						{ 0, 0, 4, 14, 10, 0, 2, 0, 0 },
-						{ 0, 0, 0, 0, 0, 2, 0, 1, 6 },
-						{ 8, 11, 0, 0, 0, 0, 1, 0, 7 },
-						{ 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
-
-	// Function call
-	dijkstra(graph, 0);
-
-	return 0;
-}
